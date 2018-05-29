e782290707f647ab306b68b4664f0966aacd9823
PuddingPudding <hwsh70119@gmail.com>
2018-05-30 01:29:45 +0800

    首次commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float m_fSpeed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) )
+        {
+            this.Movement(Vector2.up);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) )
+        {
+            this.Movement(Vector2.down);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.Movement(Vector2.left);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) )
+        {
+            this.Movement(Vector2.right);
+        }
+    }
+
+    void Movement(Vector2 _v2Dir)
+    {
+        this.transform.Translate(_v2Dir.normalized * Time.deltaTime * this.m_fSpeed);
+    }
+}
