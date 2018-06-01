331db5430b40e4b830c64aa41fd3bd496a252a37
PuddingPudding <hwsh70119@gmail.com>
2018-06-01 18:18:35 +0800

    新增造劍效果

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,6 +11,7 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private Sprite m_sprHeartBroken;
     [SerializeField] private RectTransform m_heartPos;
     [SerializeField] private int m_iCurrentHP = 5;
+    [SerializeField] private GameObject m_goSword;
     private List<Image> m_imgArrHP = new List<Image>();
 
     // Use this for initialization
@@ -45,7 +46,12 @@ public class PlayerScript : MonoBehaviour
             fAnimSpeed = 0;
         }
 
-        if(Input.GetKeyDown(KeyCode.H))
+        if(Input.GetKeyDown(KeyCode.Z) )
+        {
+            this.Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
         {
             this.m_iCurrentHP++;
             this.SetHPUI();
@@ -59,7 +65,7 @@ public class PlayerScript : MonoBehaviour
         this.Movement(v2MoveDir);
         this.m_animPlayerAnimator.SetFloat("SpeedX", v2MoveDir.x);
         this.m_animPlayerAnimator.SetFloat("SpeedY", v2MoveDir.y);
-        this.m_animPlayerAnimator.speed = fAnimSpeed;
+        this.m_animPlayerAnimator.speed = fAnimSpeed;        
     }
 
     void Movement(Vector2 _v2Dir)
@@ -67,12 +73,12 @@ public class PlayerScript : MonoBehaviour
         this.transform.Translate(_v2Dir.normalized * Time.deltaTime * this.m_fSpeed);
     }
 
-    void SetHPUI()
+    private void SetHPUI()
     {
         int iHeartNum = this.m_iCurrentHP / 2;
-        for (int i = 0; i < iHeartNum ; i++)
+        for (int i = 0; i < iHeartNum; i++)
         {
-            if (i+1 > this.m_imgArrHP.Count) //如果新生成的血量UI超出列表上限，那就額外生成
+            if (i + 1 > this.m_imgArrHP.Count) //如果新生成的血量UI超出列表上限，那就額外生成
             {
                 GameObject goTemp = new GameObject();
                 goTemp.AddComponent<Image>().sprite = this.m_sprHeart;
@@ -87,7 +93,7 @@ public class PlayerScript : MonoBehaviour
                 this.m_imgArrHP[i].gameObject.SetActive(true);
             }
         }
-        if(this.m_iCurrentHP % 2 == 1) //每兩滴血算一顆愛心，要是血量為奇數，最後會多一滴血
+        if (this.m_iCurrentHP % 2 == 1) //每兩滴血算一顆愛心，要是血量為奇數，最後會多一滴血
         {
             iHeartNum++; //愛心數量會多一，不過這一顆將會是半顆愛心
             if (iHeartNum > this.m_imgArrHP.Count) //若多出來的那一滴血超出列表範圍
@@ -95,20 +101,41 @@ public class PlayerScript : MonoBehaviour
                 GameObject goTemp = new GameObject();
                 goTemp.AddComponent<Image>().sprite = this.m_sprHeartBroken;
                 goTemp.GetComponent<RectTransform>().SetParent(this.m_heartPos);
-                goTemp.GetComponent<RectTransform>().localPosition = new Vector2(150 * (iHeartNum-1), 0);
+                goTemp.GetComponent<RectTransform>().localPosition = new Vector2(150 * (iHeartNum - 1), 0);
                 goTemp.transform.localScale = new Vector3(1, 1, 1);
                 this.m_imgArrHP.Add(goTemp.GetComponent<Image>());
             }
             else
             {
-                this.m_imgArrHP[iHeartNum-1].sprite = this.m_sprHeartBroken;
-                this.m_imgArrHP[iHeartNum-1].gameObject.SetActive(true);
+                this.m_imgArrHP[iHeartNum - 1].sprite = this.m_sprHeartBroken;
+                this.m_imgArrHP[iHeartNum - 1].gameObject.SetActive(true);
             }
         }
 
-        for(int i = iHeartNum; i<this.m_imgArrHP.Count; i++) //若原本的愛心圖示列表比現在需要顯示的愛心數量多，那就把他們全部關掉
+        for (int i = iHeartNum; i < this.m_imgArrHP.Count; i++) //若原本的愛心圖示列表比現在需要顯示的愛心數量多，那就把他們全部關掉
         {
             this.m_imgArrHP[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void Attack()
+    {
+        GameObject goSwordTemp = Instantiate(this.m_goSword, this.transform.position , this.m_goSword.transform.rotation);
+        if (this.m_animPlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("WalkingDown"))
+        {
+            goSwordTemp.transform.localEulerAngles = new Vector3(0,0,180);
+        }
+        else if (this.m_animPlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("WalkingUp"))
+        {
+            goSwordTemp.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (this.m_animPlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("WalkingLeft"))
+        {
+            goSwordTemp.transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (this.m_animPlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("WalkingRight"))
+        {
+            goSwordTemp.transform.localEulerAngles = new Vector3(0, 0, 270);
         }
     }
 }
